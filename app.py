c065fa54bc3af157519b00c4cd6ac9307c8e9723
Kai Nagar <kaynagar@gmail.com>
2023-04-02 21:49:32 +0300

    set up both endpoints

--- app.py
+++ app.py
@@ -3,5 +3,14 @@ from flask import Flask
 app = Flask(__name__)
 
 
+@app.route('/')
+def index():
+    return 'Welcome to ChatGPT API Wrapper!'
+
+# todo - move to blueprint folder 
+@app.route('/chat')
+def chat():
+    return 'this is from chat'
+
 if __name__ == '__main__':
     app.run(debug=True, port='8080')
