0d50c48cc125e09affb5622d2dd5d722f7342e26
Kai Nagar <kaynagar@gmail.com>
2023-04-02 23:30:09 +0300

    making a new blueprint for chat endpoints

--- app.py
+++ app.py
@@ -1,35 +1,13 @@
 from flask import Flask, jsonify, request
-import requests
-import json
+
+from chat import chat
 
 app = Flask(__name__)
-
+app.register_blueprint(chat,url_prefix='/chat')
 
 @app.route('/')
 def index():
     return 'Welcome to ChatGPT API Wrapper!'
-
-# todo - move to blueprint folder
-
-
-@app.route('/chat', methods=['POST'])
-def chat():
-    try:
-        message = request.json['message']
-    except KeyError:
-        return jsonify({'error': 'No message was sent.'}), 400
-
-    #  buidling the request to the chatGPT api
-    chatgpt_url = "https://api-inference.huggingface.co/models/EleutherAI/gpt-neo-2.7B"
-    try:
-        response = requests.post(chatgpt_url, message)
-        result = json.loads(response.content.decode('utf-8'))
-        generated_text = result[0]["generated_text"]
-        return generated_text
-    except requests.exceptions.HTTPError as e:
-        return jsonify({'error':str(e)})
-
-
 
 if __name__ == '__main__':
     app.run(debug=True, port='8080')
---
+++ chat.py
@@ -0,0 +1,33 @@
+from flask import Blueprint, request, jsonify, redirect,url_for
+import requests
+import json
+
+chat = Blueprint(__name__,'chat')
+
+
+@chat.route('/', methods=['POST'])
+def chat_post():
+    try:
+        message = request.json['message']
+    except KeyError:
+        return jsonify({'error': 'No message was sent.'}), 400
+
+    chatgpt_url = "https://api-inference.huggingface.co/models/EleutherAI/gpt-neo-2.7B"
+    headers = {
+        'Content-Type': 'application/json'
+    }
+    data = {
+        'inputs': message,
+        'options': {
+            # 'generate_length': 50, # can add answer length here
+            'max_time': 10
+        }
+    }
+    try:
+        response = requests.post(chatgpt_url, headers=headers, json=data)
+        result = json.loads(response.content.decode('utf-8'))
+        generated_text = result[0]["generated_text"]
+        return generated_text
+    except requests.exceptions.HTTPError as e:
+        return jsonify({'error':str(e)})
+
