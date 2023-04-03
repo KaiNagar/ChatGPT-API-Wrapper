c7ccd76779642c35674b26359a7043d5992ebc5d
Kai Nagar <kaynagar@gmail.com>
2023-04-03 12:08:47 +0300

    fixed cors default port to match both

--- backend/app.py
+++ backend/app.py
@@ -4,14 +4,15 @@ from flask_cors import CORS
 from chat import chat
 
 app = Flask(__name__)
-CORS(app, origins=['http://localhost:3000'])
+CORS(app, origins=['http://localhost:3000','http://127.0.0.1:3000'])
 app.register_blueprint(chat, url_prefix='/chat')
 
 
 @app.route('/')
 def index():
+    print('hey')
     return 'Welcome to ChatGPT API Wrapper!'
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port='8080')
+    app.run(debug=True, port='5000')
--- frontend/src/services/chatService.js
+++ frontend/src/services/chatService.js
@@ -5,7 +5,7 @@ export const chatService = {
     sendMsg
 }
 
-const API_URL = 'http://127.0.0.1:8080'
+const API_URL = 'http://127.0.0.1:5000/'
 
 async function getHomeMsg() {
     try{
