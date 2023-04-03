5978093f352d4bab1624d3d85449b13c6984d3b7
Kai Nagar <kaynagar@gmail.com>
2023-04-03 13:43:23 +0300

    adding comments in backend

--- backend/app.py
+++ backend/app.py
@@ -1,18 +1,24 @@
 from flask import Flask
+
+# importing cors for connecting the frontend
 from flask_cors import CORS
 
+# made a diffrent file for the chat endpoints for future expantion and to make the app.py file more readable and specific
 from chat import chat
 
 app = Flask(__name__)
+# added 2 origins to work with this backend using cors, if not specified endpoints could be used from any origin
 CORS(app, origins=['http://localhost:3000','http://127.0.0.1:3000'])
+
+# making all the chat endpoints start with /chat to make the code more orgenized
 app.register_blueprint(chat, url_prefix='/chat')
 
-
+# first endpoint requested at '/' returning a simple text , this is presented at the frontend in the home page
 @app.route('/')
 def index():
-    print('hey')
     return 'Welcome to ChatGPT API Wrapper!'
 
 
+# for development used debug true to reload after every change
 if __name__ == '__main__':
     app.run(debug=True, port='5000')
--- backend/chat.py
+++ backend/chat.py
@@ -4,28 +4,41 @@ import json
 
 chat = Blueprint(__name__, 'chat')
 
-
+# second endpoint requested at /chat, i use prefix `/chat` so here all we need is a simple '/'
 @chat.route('/', methods=['POST'])
 def chat_post():
+    # catching the message from request body,if there is no message will return an error in json format to be collected at the front end
     try:
         message = request.json['message']
     except KeyError:
         return jsonify({'error': 'No message was sent.'}), 400
 
+    #bulding the chat request with chatgpt URL 
     chatgpt_url = "https://api-inference.huggingface.co/models/EleutherAI/gpt-neo-2.7B"
+    #Headers (can add here authorization with API key but this is a free API so there is no need for that)
     headers = {
         'Content-Type': 'application/json'
     }
+    # and data - for longer response i set the response length to 500 characters
     data = {
         'inputs': message,
         'options': {
-            'generate_length': 500,  # for max length per response i set it to 500 characters
+            'generate_length': 500,  
             'max_time': 10
         }
     }
+
+    # request it self, if at any stage something went wrong it will catch the error
+    # and throw it back to the frontend for the user to know somethign went wrong
     try:
+        # making a post request with url headers and data
         response = requests.post(chatgpt_url, headers=headers, json=data)
+
+        # after getting the respone converting the json formatted string into a python dictionary object
+        # then taking the contect and converting into a string object using 'utf-8' encoding
         result = json.loads(response.content.decode('utf-8'))
+        
+        # taking only the text inself from the result and returning it to the frontend
         generated_text = result[0]["generated_text"]
         return generated_text
     except requests.exceptions.HTTPError as e:
