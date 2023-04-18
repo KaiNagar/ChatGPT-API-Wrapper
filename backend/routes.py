c9fcd21c4a15a84d0573d53d05b9b892579376d4
kai nagar laptop <kaynagar@gmail.com>
2023-04-18 07:59:21 -0400

    changed user msg in game prompt to fit boolean arg

--- backend/routes.py
+++ backend/routes.py
@@ -46,32 +46,32 @@ data = [
         }
     },
     # this route is mainly to check boolean arg flow
-    # {
-    #     "path": "game",
-    #     "args": [
-    #         {
-    #             "name": "team",
-    #             "type": "string"
-    #         },
-    #         {
-    #             "name": "date",
-    #             "type": "date-time"
-    #         },
-    #         {
-    #             "name":"won",
-    #             "type":"boolean"
-    #         }
-    #     ],
-    #     "prompt": {
-    #         "messages": [
-    #             {
-    #                 "role": "system",
-    #                 "content": "You are the game service. Please provide the game status for {team} on {date}."
-    #             },                {
-    #                 "role": "user", "content": "Can you please provide the game status for {team} on {date}"}
+    {
+        "path": "game",
+        "args": [
+            {
+                "name": "team",
+                "type": "string"
+            },
+            {
+                "name": "date",
+                "type": "date-time"
+            },
+            {
+                "name":"won",
+                "type":"boolean"
+            }
+        ],
+        "prompt": {
+            "messages": [
+                {
+                    "role": "system",
+                    "content": "You are the game service. Please provide the game status for {team} on {date} if they won."
+                },                {
+                    "role": "user", "content": "Can you please provide the game status for {team} on {date}, and they won? {won}"}
 
-    #         ]
-    #     }
-    # }
+            ]
+        }
+    }
    
 ]
