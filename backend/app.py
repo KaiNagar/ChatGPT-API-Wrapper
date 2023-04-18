e8272a1d565ae0a5403a08d174a01931622b0378
kai nagar laptop <kaynagar@gmail.com>
2023-04-18 07:28:32 -0400

    improved code to be more dynamic with args

--- backend/app.py
+++ backend/app.py
@@ -20,12 +20,16 @@ app.register_blueprint(chat, url_prefix='/chat')
 app.register_blueprint(product, url_prefix='/product')
 
 # first endpoint requested at '/' returning a simple text, this is presented at the frontend on the home page
+
+
 @app.route('/')
 def index():
     return 'Welcome to ChatGPT API Wrapper!'
 
 # function that will create each endpoint
-def create_view_func( args, prompt):
+
+
+def create_view_func(args, prompt):
     # to prevent closure problems we need to create for each endpoints a new view_func with a new pointer
     def view_func():
         # Parse the request arguments
@@ -44,6 +48,7 @@ def create_view_func( args, prompt):
                 value = float(request.args.get(name))
             elif arg_type == 'boolean':
                 value = request.args.get(name).lower() == 'true'
+                print(value)
             elif arg_type == 'date-time':
                 value = request.args.get(name)
             request_args[name] = value
@@ -52,15 +57,16 @@ def create_view_func( args, prompt):
         response = {}
         for msg in prompt["messages"]:
             role = msg["role"]
-            content = msg["content"].format(
-                date=request_args.get("date", ""),
-                location=request_args.get("location", "")
-            )
+            content = msg["content"]
+            for arg in args:
+                arg_name = arg["name"]
+                arg_value = request_args.get(arg_name, "")
+                content = content.replace(f"{{{arg_name}}}", str(arg_value))
             response[role] = content
-
         return jsonify(response)
 
     return view_func
+
 
 # creating an endpoint for each item in the data array
 for item in data:
@@ -69,9 +75,10 @@ for item in data:
     prompt = item.get('prompt')
 
     # this line creates a new endpoint using the function above for each path specified in data
-    # right now all methods are GET by default but we can add to each route a property specifying what kind of method it is 
+    # right now all methods are GET by default but we can add to each route a property specifying what kind of method it is
     # then extract it from the request and use it in here
-    app.add_url_rule(f'/{path}', view_func=create_view_func(args,prompt), methods=['GET'], endpoint=path)
+    app.add_url_rule(f'/{path}', view_func=create_view_func(args,
+                     prompt), methods=['GET'], endpoint=path)
 
 # for development, use debug mode to automatically reload the server after every change
 if __name__ == '__main__':
